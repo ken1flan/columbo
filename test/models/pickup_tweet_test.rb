a37c82e8fe65f8a83e13d227052c1365a048cf46
ken1flan <ken1flan@gmail.com>
2014-07-23 15:48:22 +0900

    refs #56 `default_scope`、`.can_display`のテストを書いた。

--- test/models/pickup_tweet_test.rb
+++ test/models/pickup_tweet_test.rb
@@ -2,12 +2,35 @@ require 'test_helper'
 
 describe PickupTweet do
   describe 'default_scope' do
-    it 'test' do
-      t = build(:pickup_tweet)
+    context '1〜3日前のツイートが登録されているとき' do
+      before do
+        @tweet1 = create(:pickup_tweet, tweet_at: 1.day.ago)
+        @tweet2 = create(:pickup_tweet, tweet_at: 2.day.ago)
+        @tweet3 = create(:pickup_tweet, tweet_at: 3.day.ago)
+        @tweets = PickupTweet.all
+      end
+
+      it 'ツイート時間が古い順に選択されること' do
+        @tweets[0].id = @tweet3
+        @tweets[1].id = @tweet2
+        @tweets[2].id = @tweet1
+      end
     end
   end
 
   describe '.can_display' do
+    context '削除済みのツイートとそうでないツイートがあるとき' do
+      before do
+        @tweet = create(:pickup_tweet)
+        @truncated_tweet = create(:pickup_tweet, :truncated)
+        @tweets = PickupTweet.can_display
+      end
+
+      it '削除済みのツイートは選択されないこと' do
+        @tweets.select{|t| t.id == @tweet.id}.present?.must_equal true
+        @tweets.select{|t| t.id == @truncated_tweet.id}.blank?.must_equal true
+      end
+    end
   end
 
   describe '#is_liked_by?' do
