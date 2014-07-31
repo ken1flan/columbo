6e3d3042ba4d2e85442a974b1ab2c45dfb20ad60
ken1flan <ken1flan@gmail.com>
2014-07-31 09:13:28 +0900

    refs #54 userのテストの項目

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe User do
+  describe '.find_for_oauth' do
+    context 'authが未登録の情報のとき' do
+      context 'signed_in_resourceがnilのとき' do
+        it '登録されること' do
+        end
+      end
+
+      context 'signed_in_resourceがauthで指定されているものと違うとき' do
+      end
+
+      context 'signed_in_resourceがauthで指定されているものと同じとき' do
+      end
+    end
+
+    context 'authが登録済の情報のとき' do
+      context 'signed_in_resourceがnilのとき' do
+      end
+
+      context 'signed_in_resourceがauthで指定されているものと違うとき' do
+      end
+
+      context 'signed_in_resourceがauthで指定されているものと同じとき' do
+      end
+     end
+  end
 end
