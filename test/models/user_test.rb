3e9f6476768200c0996d9847b64e5e1acb6eec34
ken1flan <ken1flan@gmail.com>
2014-08-02 10:04:10 +0900

    テストの実装 User::.find_for_oauth::userの紐付いているidentityをauthに指定しているとき::signed_in_resourceがnilのとき   test_0001_identity、userは登録されないこと User::.find_for_oauth::identityに未登録のauthのとき::signed_in_resourceに登録されているuserが指定されているとき   test_0001_userは増えず、identityは登録されること

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 describe User do
   describe '.find_for_oauth' do
-    context 'authが未登録の情報のとき' do
+    context 'identityに未登録のauthのとき' do
       before do
         @auth = OmniAuth::AuthHash.new(
           provider: 'provider',
@@ -16,14 +16,63 @@ describe User do
       end
 
       context 'signed_in_resourceがnilのとき' do
-        it '登録されること' do
+        it 'authの情報が、identityとuserに登録されること' do
           user = User.find_for_oauth( @auth, nil )
           identity = Identity.find_by( uid: @auth.uid )
+
+          user.name.must_equal(@auth.extra.raw_info.name)
+          identity.provider.must_equal(@auth.provider)
+          identity.uid.must_equal(@auth.uid)
+          identity.user_id.must_equal(user.id)
         end
       end 
+
+      context 'signed_in_resourceに登録されているuserが指定されているとき' do
+        before do
+          @user = create(:user)
+          @user_count = User.count
+        end
+
+        it 'userは増えず、identityは登録されること' do
+          user = User.find_for_oauth( @auth, @user )
+          identity = Identity.find_by( uid: @auth.uid )
+
+          User.count.must_equal(@user_count)
+          user.must_equal(@user)
+          identity.provider.must_equal(@auth.provider)
+          identity.uid.must_equal(@auth.uid)
+          identity.user_id.must_equal(user.id)
+        end
+
+      end
     end
 
-    context 'ユーザの紐付いているidentityをauthに指定しているとき' do
+    context 'userの紐付いているidentityをauthに指定しているとき' do
+      before do
+        @user = create(:user)
+        @identity = create(:identity, user_id: @user.id)
+        @auth = OmniAuth::AuthHash.new(
+          provider: @identity.provider,
+          uid: @identity.uid,
+          extra: {
+            raw_info: {
+              name: 'test_name'
+            }
+          }
+        )
+        @user_count = User.count
+        @identity_count = Identity.count
+      end
+
+      context 'signed_in_resourceがnilのとき' do
+        it 'identity、userは登録されないこと' do
+          user = User.find_for_oauth( @auth, nil )
+
+          User.count.must_equal(@user_count)
+          Identity.count.must_equal(@identity_count)
+          user.must_equal(@user)
+        end
+      end
     end
 
     context 'ユーザの紐付いていないidentityをauthに指定しているとき' do
