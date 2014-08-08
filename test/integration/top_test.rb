0496e4e1a3204241869ca536852e000d04cad43a
ken1flan <ken1flan@gmail.com>
2014-08-08 20:25:29 +0900

    topのintegration項目だけ

--- test/integration/top_test.rb
+++ test/integration/top_test.rb
@@ -3,9 +3,35 @@ require 'test_helper'
 describe 'トップページ Integration' do
   before do
     create_list(:pickup_tweet, 10)
+    @target = create(:pickup_tweet)
+    visit root_path
   end
 
-  it 'test' do
-    visit root_path
+  it 'ツイートが表示されていること' do
+    @page.text.must_include(@target.
+  end
+
+  context 'ログインしているとき' do
+    context 'スターを押したとき' do
+      it 'スターが黒くなること' do
+        skip
+      end
+
+      it 'スターの横の数値が1になること' do
+        skip
+      end
+    end
+  end
+
+  context 'ログインしているとき' do
+    context 'スターを押したとき' do
+      it 'スターが黒くなること' do
+        skip
+      end
+
+      it 'スターの横の数値が1になること' do
+        skip
+      end
+    end
   end
 end
