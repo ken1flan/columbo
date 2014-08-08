1d1ae9792063233e78d0a177ddc903be92a04078
ken1flan <ken1flan@gmail.com>
2014-08-08 09:12:19 +0900

    facebookログインのテストを追加

--- test/integration/user_authentication_test.rb
+++ test/integration/user_authentication_test.rb
@@ -9,11 +9,11 @@ describe 'ユーザ認証 Integration' do
   describe 'twitterログイン' do
     before do
       OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-        provider: 'provider',
-        uid: 'test_uid',
+        provider: 'twitter',
+        uid: 'twitter_uid',
         extra: {
           raw_info: {
-            name: 'test_name'
+            name: 'twitter_user_name'
           }
         }
       })
@@ -26,7 +26,43 @@ describe 'ユーザ認証 Integration' do
       end
 
       it 'ニックネームが表示されること' do
-        page.text.must_include('test_name')
+        page.text.must_include('twitter_user_name')
+      end
+
+      context 'ログアウトを押したとき' do
+        before do
+          click_link 'ログアウト'
+        end
+
+        it '各ログインボタンが表示されること' do
+          page.text.must_include('twitterログイン')
+          page.text.must_include('facebookログイン')
+        end
+      end
+    end
+  end
+
+  describe 'facebookログイン' do
+    before do
+      OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+        provider: 'facebook',
+        uid: 'facebook_uid',
+        extra: {
+          raw_info: {
+            name: 'facebook_user_name'
+          }
+        }
+      })
+    end
+
+    context 'トップページ上部のfacebookログインをクリックしたとき' do
+      before do
+        visit root_path
+        click_link 'facebookログイン'
+      end
+
+      it 'ニックネームが表示されること' do
+        page.text.must_include('facebook_user_name')
       end
 
       context 'ログアウトを押したとき' do
