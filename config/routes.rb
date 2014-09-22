b96a1fe01087df56a16aa9ab4c8e491eeee8b59f
ken1flan <ken1flan@gmail.com>
2014-09-22 09:40:16 +0900

    rakeタスク、cron用の口の準備

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   # for poor man's cron
   get 'cron/pickup_tweets' => 'cron#pickup_tweets'
   get 'cron/housekeep_tweets' => 'cron#housekeep_tweets'
+  get 'cron/pickup_tweets_per_day' => 'cron#pickup_tweets_per_day'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
