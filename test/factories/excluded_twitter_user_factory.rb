f598c8eba1ffdb09578a1731bad8c886bbcd3f6d
ken1flan <ken1flan@gmail.com>
2014-09-24 09:04:24 +0900

    refs #64 アノテーションの追加

--- test/factories/excluded_twitter_user_factory.rb
+++ test/factories/excluded_twitter_user_factory.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: excluded_twitter_users
+#
+#  id          :integer          not null, primary key
+#  uid         :string(255)
+#  name        :string(255)
+#  screen_name :string(255)
+#  memo        :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 FactoryGirl.define do
   factory :excluded_twitter_user do
     uid { rand(10000000).to_s }
